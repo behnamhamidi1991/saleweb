2b1018797a88bbdfb5855067ea22cbfa07fb5b71
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-19 18:06:17 +0330

    Navbar

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,19 @@ export default function Home() {
   return (
     <div className="pageContainer">
       <Homepage />
+      <div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
     </div>
   );
 }
--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import styles from "./homepage.module.css";
+import { FaLocationDot } from "react-icons/fa6";
 
 const Homepage = () => {
   return (
@@ -8,12 +9,31 @@ const Homepage = () => {
       <div className={styles.navbar}>
         <Navbar />
       </div>
-      <div className={styles.content}>
-        <div className={styles.header}>
-          <h1>Largest Classified In The World</h1>
-          <p>You can sell or by anything you want</p>
+      <div className={styles.homePageContainer}>
+        <div className={styles.content}>
+          <div className={styles.header}>
+            <h1>Largest Classified In The World</h1>
+            <p>You can sell or by anything you want</p>
+          </div>
+          <div className={styles.searchBar}>
+            <input type="text" placeholder="What are you looking for?" />
+            <div className={styles.locationContainer}>
+              <input type="text" placeholder="Location" />
+              <FaLocationDot className={styles.locationIcon} />
+            </div>
+            <select>
+              <option>All Categories</option>
+              <option>Real State</option>
+              <option>Books & Magazines</option>
+              <option>Furniture</option>
+              <option>Electronics</option>
+              <option>Cars & Vehicles</option>
+              <option>Others</option>
+            </select>
+
+            <button className={styles.searchBtn}>Search</button>
+          </div>
         </div>
-        <div className={styles.searchBar}></div>
       </div>
     </div>
   );
