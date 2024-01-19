2e6c52144a7967aedc51fe0d898d9f38d0babf23
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-19 16:55:38 +0330

    Parallax

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
-import Homepage from "@/components/Homepage/Homepage";
-import Homepage2 from "@/components/Homepage2/Homepage2";
-
 export default function Home() {
   return (
-    <div>
-      <Homepage />
-      <Homepage2 />
+    <div className="container">
+      <div className="box1">
+        <h1>Easy Parallax Effect</h1>
+      </div>
+
+      <div className="box2">
+        <h1>Section Two Parallax</h1>
+      </div>
+      <div className="box3">
+        <h1>Section Three Parallax</h1>
+      </div>
     </div>
   );
 }
