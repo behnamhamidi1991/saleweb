9942b1a2e58709496ef899b2f2560564e853580d
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-20 16:09:14 +0330

    Burger Menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,34 +1,39 @@
 import React from "react";
 import styles from "./navbar.module.css";
 import Link from "next/link";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { RxCross1 } from "react-icons/rx";
 
 const Navbar = () => {
   return (
-    <div className={styles.navContainer}>
-      <div className={styles.logo}>
-        CLASSY<span>ADS</span>{" "}
-      </div>
-      <ul className={styles.menu}>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/">Blog</Link>
-        </li>
-        <li>
-          <Link href="/">Categories</Link>
-        </li>
-        <li>
-          <Link href="/">Contact Us</Link>
-        </li>
-        <li>
-          <Link href="/">About Us</Link>
-        </li>
-      </ul>
+    <div>
+      <div className={styles.navContainer}>
+        <div className={styles.logo}>
+          CLASSY<span>ADS</span>{" "}
+        </div>
+        <ul className={styles.menu}>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/">Blog</Link>
+          </li>
+          <li>
+            <Link href="/">Categories</Link>
+          </li>
+          <li>
+            <Link href="/">Contact Us</Link>
+          </li>
+          <li>
+            <Link href="/">About Us</Link>
+          </li>
+        </ul>
 
-      <div className={styles.adBtn}>
-        <button>Create An Ad</button>
+        <div className={styles.adBtn}>
+          <button>Create An Ad</button>
+        </div>
       </div>
+      <RxHamburgerMenu className={styles.burgerIcon} />
     </div>
   );
 };
