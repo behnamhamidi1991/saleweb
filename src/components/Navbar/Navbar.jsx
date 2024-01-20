19a326b138dd9d4e4890d104d50c2a89f8927cd8
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-20 12:34:00 +0330

    Menu items

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { Menu } from "antd";
-import { IoIosArrowDown } from "react-icons/io";
 import styles from "./navbar.module.css";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
@@ -9,133 +8,23 @@ const Navbar = () => {
       <div className={styles.logo}>
         CLASSY<span>ADS</span>{" "}
       </div>
-      <Menu
-        mode="horizontal"
-        className={styles.menu}
-        items={[
-          {
-            label: "Home",
-            key: "Home",
-          },
-          {
-            label: "Blog",
-            key: "Blog",
-          },
-          {
-            label: "Categories",
-            key: "Categories",
-            icon: <IoIosArrowDown />,
-            children: [
-              {
-                label: "Cars",
-                key: "Cars",
-              },
-              {
-                label: "Digital",
-                key: "Digital",
-                children: [
-                  {
-                    label: "Laptop",
-                    key: "Laptop",
-                  },
-                  {
-                    label: "PC",
-                    key: "PC",
-                  },
-                  {
-                    label: "Mobile",
-                    key: "Mobile",
-                  },
-                  {
-                    label: "TV",
-                    key: "TV",
-                  },
-                  {
-                    label: "Radio",
-                    key: "Radio",
-                  },
-                  {
-                    label: "Speaker",
-                    key: "Speaker",
-                  },
-                  {
-                    label: "Headphones",
-                    key: "Headphones",
-                  },
-                  {
-                    label: "Router",
-                    key: "Router",
-                  },
-                  {
-                    label: "Receiver",
-                    key: "Receiver",
-                  },
-                ],
-              },
-              {
-                label: "Clothes",
-                key: "Clothes",
-                children: [
-                  {
-                    label: "Men",
-                    key: "Men",
-                  },
-                  {
-                    label: "Women",
-                    key: "Men",
-                  },
-                  {
-                    label: "Kids",
-                    key: "Men",
-                  },
-                ],
-              },
-              {
-                label: "Furniture",
-                key: "Furniture",
-              },
-              {
-                label: "Study",
-                key: "Study",
-              },
-              {
-                label: "Accessory",
-                key: "Accessory",
-              },
-              {
-                label: "Kitchen",
-                key: "Kitchen",
-              },
-              {
-                label: "Sport",
-                key: "Sport",
-              },
-              {
-                label: "Watch",
-                key: "Watch",
-              },
-              {
-                label: "Apartment",
-                key: "Apartment",
-              },
-            ],
-          },
-          {
-            label: "Contact",
-            key: "Contact",
-          },
-          {
-            label: "About",
-            key: "About",
-          },
-
-          {
-            label: "Post An Ad",
-            key: "Post_Ad",
-            className: "postBtn",
-          },
-        ]}
-      ></Menu>
+      <ul className={styles.menu}>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/">Blog</Link>
+        </li>
+        <li>
+          <Link href="/">Categories</Link>
+        </li>
+        <li>
+          <Link href="/">Contact Us</Link>
+        </li>
+        <li>
+          <Link href="/">About Us</Link>
+        </li>
+      </ul>
     </div>
   );
 };
