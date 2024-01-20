f8b8a992c10d798a964ac80b7844b6e1e4dad721
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-20 22:28:33 +0330

    Categories

--- src/components/Trend/Trend.jsx
+++ src/components/Trend/Trend.jsx
@@ -5,16 +5,25 @@ import Image from "next/image";
 import { CiHeart } from "react-icons/ci";
 
 const Trend = () => {
-  const newFeaturedAds = featuredAds.filter((ad) => ad.id >= 6);
+  const newFeaturedAds = featuredAds.filter((ad) => ad.id <= 6);
 
   return (
     <div className={styles.trendContainer}>
-      <div className={styles.header}></div>
+      <div className={styles.header}>
+        <h2>Trending Today</h2>
+        <p>Take a look at our trending posts</p>
+      </div>
       <div className={styles.trendItemsContainer}>
         {newFeaturedAds.map((ad) => (
-          <div key={ad.id}>
-            <div className={styles.image}>
-              <Image src={ad.imgUrl} width={400} height={400} alt={ad.title} />
+          <div key={ad.id} className={styles.box}>
+            <div className={styles.imageContainer}>
+              <Image
+                src={ad.imgUrl}
+                width={400}
+                height={400}
+                alt={ad.title}
+                className={styles.image}
+              />
             </div>
             <div className={styles.content}>
               <div className={styles.contentHeader}>
@@ -26,7 +35,7 @@ const Trend = () => {
                 <p className={styles.location}>{ad.location}</p>
               </div>
               <div>
-                <p className={styles.review}>{ad.reviews}</p>
+                <p className={styles.review}>{ad.reviews} Reviews</p>
               </div>
             </div>
           </div>
